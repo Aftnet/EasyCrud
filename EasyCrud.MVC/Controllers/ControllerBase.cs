a7863f0a40e6e990cd848e441a7af16cc0973037
Alberto Fustinoni <alberto@aftnet.net>
2015-09-15 23:05:12 +0900

    Added controller infrastructure for MVC

---
+++ EasyCrud.MVC/Controllers/ControllerBase.cs
@@ -0,0 +1,26 @@
+﻿using EasyCrud.Logging;
+using System;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyCrud.MVC.Controllers
+{
+    public class ControllerBase : Controller
+    {
+        public const string InternalServerErrorMsg = "Internal server error";
+
+        protected readonly ILogger Logger;
+
+        public ControllerBase(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        protected void RaiseErrorMessage(HttpStatusCode statusCode, string errorMessage, LogSeverity severity, Exception exception = null)
+        {
+            Logger.Log(severity, errorMessage, exception);
+            throw new HttpException((int)statusCode, errorMessage);
+        }
+    }
+}
